bf069132e9003e85e7414f33a21436e888cc05b8
msecreative <47583511+msecreative@users.noreply.github.com>
2021-12-21 15:05:53 +0600

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2hhcm1pbjQyODkiLCJhIjoiY2t4ZXc1c2trM3kwejJ2bXVpaWhtbW9jMyJ9.IfWdwEE8d37ZCfPEdNMyEg';
+            var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-71.060982, 42.35725],
+            zoom: 18
+            });
